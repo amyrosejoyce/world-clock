86163afb7e5fed84e1e34e6562d5b8809d992ed3
amyrosejoyce <150541286+amyrosejoyce@users.noreply.github.com>
2024-01-29 16:28:43 +0000

    updated function to change clicked on city time and relplace display

--- Javascript/index.js
+++ Javascript/index.js
@@ -24,4 +24,23 @@ function timeRefreash() {
     .format("dddd,MMM D,YYYY");
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city" id="los-angeles">
+          <div>
+            <h2>${cityTimeZone}</h2>
+            <div class=${format("dddd,MMM D,YYYY")}
+          </div>
+          <div class=${("hh:mm:ss [<small>]A[</small]");><small>AM</small>}</div>`;
+              
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
+
+
+
 setInterval(timeRefreash, 1000);
