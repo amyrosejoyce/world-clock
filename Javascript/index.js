ddb873f983884d83d97c6ee7fa174c89c13a644b
amyrosejoyce <150541286+amyrosejoyce@users.noreply.github.com>
2024-01-29 13:22:16 +0000

    created basic JS and added timezones and live time

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,25 @@
+function timeRefreash() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = document.querySelector("#los-angeles .date");
+  let losAngelesTimeElement = document.querySelector("#los-angeles .time");
 
+  losAngelesTimeElement.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("hh:mm:ss A");
+
+  losAngelesDateElement.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("dddd,MMM D,YYYY");
+
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = document.querySelector("#paris .date");
+  let parisTimeElement = document.querySelector("#paris .time");
+
+  parisTimeElement.innerHTML = moment().tz("Europe/Paris").format("hh:mm:ss A");
+
+  parisDateElement.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("dddd,MMM D,YYYY");
+}
+
+setInterval(timeRefreash, 1000);
